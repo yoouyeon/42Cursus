275bac9cc49200c09a7766278adf3895559bdf40
yoouyeon <jyeon.yoon59@gmail.com>
2022-07-30 15:55:11 +0900

    :bulb: [CPP Module] 평가 이후 알게 된 내용 주석 추가

--- CPP_Module/CPP Module 07/ex00/main.cpp
+++ CPP_Module/CPP Module 07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jeyoon <jeyoon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 03:02:27 by jeyoon            #+#    #+#             */
-/*   Updated: 2022/07/29 14:51:35 by jeyoon           ###   ########seoul.kr  */
+/*   Updated: 2022/07/30 15:35:57 by jeyoon           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int main( void ) {
 	int a = 2;
 	int b = 3;
 
-	::swap( a, b );
+	::swap( a, b ); // :: 연산자 : 전역으로 선언되어 있는(헤더파일에 정의했던) swap 함수를 사용하겠다는 의미
 
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
@@ -31,6 +31,18 @@ int main( void ) {
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	/*
+		const type에 대한 고민도 했었으면 더 좋았을 것 같다.
+	*/
+
+	const float e = 4.2;
+	const float f = 42.42;
+
+	//::swap(e, f);
+	std::cout << "e = " << c << ", f = " << d << std::endl;
+	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
 
 	return 0;
 }
